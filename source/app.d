af4c3bc4e6634e5971c81e4a856a43310f8f4774
matt <mhrawcliffe@gmail.com>
2019-07-17 17:00:50 +0100

    use env vars for easier dev

--- source/app.d
+++ source/app.d
@@ -3,12 +3,25 @@ import std.stdio : writeln;
 
 void main()
 {
+  import std.process : environment;
+
   import matrix.api;
   import matrix.api.login;
 
-  string baseUrl = "https://matrix.org/";
+  string serv = environment.get("SW_SERV");
+  string user = environment.get("SW_USER");
+  string pass = environment.get("SW_PASS");
 
-  auto tid = spawn(&execute!Login, baseUrl, "", "");
+  if (serv.length == 0 || user.length == 0 || pass.length == 0) {
+    writeln("Missing dev environment vars");
+    return;
+  }
+
+  // Might be ok to spawn a thread for these events so long as there is
+  // a long running one for sync
+  // ^ Perhaps use a template flag + static if for whether to use a while
+  // loop in executeRequest
+  auto tid = spawn(&execute!Login, serv, user, pass);
 
   receive(
     (Response!Login res) {
