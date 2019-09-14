b8e05f74b8ebde2b569120425fd849fb70d5e59d
matt <a@b5.re>
2019-09-14 12:31:28 +0100

    Add models for sync response

---
+++ source/matrix/model/sync.d
@@ -0,0 +1,25 @@
+module matrix.model.sync;
+
+struct SyncModel
+{
+  string nextBatch;
+  RoomsModel rooms;
+  PrecenseModel precense;
+  AccountDataModel accountData;
+}
+
+struct AccountDataModel
+{
+}
+
+struct PresenceModel
+{
+}
+
+struct RoomsModel
+{
+}
+
+struct EventModel
+{
+}
